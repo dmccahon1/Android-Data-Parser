cb76e52f85319d3b594341d0deb1e51f0e424c3a
David <dmccahon1@gmail.com>
2018-05-20 13:20:50 +0100

    Main Update
    Add clear folder function, add dbsearch to main

--- code/main.py
+++ code/main.py
@@ -1,8 +1,19 @@
+import shutil
 from acquisition import adbExtract
+from search import dbSearch
+
+
+def clearFolders():
+    '''Clears androidbackup, rawdump and evidence folders'''
+    shutil.rmtree('androidbackup')
+    shutil.rmtree('rawdump')
+    shutil.rmtree('evidence')
 
 
 def main():
+    clearFolders()
     adbExtract()
+    dbSearch()
 
 
 if __name__ == '__main__':
