079baeefe911b88640d04020a0d038c02820db29
David <dmccahon1@gmail.com>
2018-09-04 10:25:39 +0100

    Update to file read, enhanced to only read the number of bytes required

--- code/acquisition.py
+++ code/acquisition.py
@@ -7,24 +7,30 @@ import os
 import datetime
 import subprocess
 import shutil
-
+# TODO: Fix date & time printing the same to file
 dt = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
 dt = dt+":\t "
 report = open("report.txt", "w+", 1)
 print(dt+"Script Started", file=report)
 
+# Platform Tools
+homePath = os.path.abspath(os.path.join(__file__, "../../platform-tools"))
+adb = homePath+'/adb'
+abe = homePath+'/abe.jar'
+unzip = homePath+'/7za.exe'
+
 # Dictionaries
 fileFound = {}
 filePath = {}
 dupFiles = {}
-fileSig = {"PNG": "89 50 4E 47",
-                "JPEG": "FF D8 FF E0",
-                "AVI": "52 48 46 46",
-                "DB": "53 51 4C 69 74 65 20 66 6F 72 6D 61 74 20 33 00",
-                "MP3": "49 44 33",
-                "MOV": "6D 6F 6F 76",
-                "TIFF": "49 49 2A",
-                "GIF": 	"47 49 46 38"}
+fileSig = {"PNG": "89504E47",
+            "JPEG": "FFD8FFE0",
+            "AVI": "52484646",
+            "DB": "53514C69746520666F726D6174203300",
+            "MP3": "494433",
+            "MOV": "6D6F6F76",
+            "TIFF": "49492A",
+            "GIF": 	"47494638"}
 
 
 def clearFolders():
@@ -42,10 +48,7 @@ def adbExtract():
     print("\n#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*\n", file=report)
     print("                  Acquisition Stage\n", file=report)
     print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*\n", file=report)
-    # UPGRADE TOOLS TO ABSOLUTE PATH
-    adb = './platform-tools/adb'
-    abe = './platform-tools/abe.jar'
-    unzip = './platform-tools/7za.exe'
+
     # Create Directory for ADB/Tar to go
     # If Directory Exists Delete .ab and .tar files for fresh extraction
     try:
@@ -55,21 +58,14 @@ def adbExtract():
         if not os.path.isdir("android_backup"):
             raise
 
-    # List Attached Devices
-    # os.system("start cmd.exe @cmd cd ./platform-tools & adb devices -l & exit")
-    subprocess.call([adb, "devices", "-l"], stdout=report)
-
     print(dt+" Extracting Backup From Android Device\n", file=report)
     # Create ADB File into directory ./android_backup
-    # os.system("start cmd.exe @cmd /k cd ./platform-tools & adb backup -apk -shared -all -f E:/Dropbox/cyber_security/yr4/honours/script/android_backup/backup.ab & exit")
     subprocess.call([adb, "backup", "-apk", "-shared", "-all", "-f", "android_backup/backup.ab"])
 
     if os.path.isfile("android_backup/backup.ab") and os.path.getsize("android_backup/backup.ab") >= 1:
         print(dt+" Android Backup Successfully Created", file=report)
         # Convert .ab file to .Tar
         print(dt, "Converting Android backup to Tar", file=report)
-        # os.system("start cmd.exe @cmd cd E:\Dropbox\cyber_security\yr4\honours\script & java -jar abe.jar unpack ./android_backup/backup.ab ./android_backup/backup.tar")
-        # SILENECE OUTPUT OF EXTRACTION
         subprocess.call(["java", "-jar", abe, "unpack", "android_backup/backup.ab", "android_backup/backup.tar"])
         if os.path.isfile("android_backup/backup.tar"):
             print(dt+" Android Backup Successfully Converted to Tar", file=report)
@@ -102,16 +98,17 @@ def fileSigAnalysis(folder):
         for file in files:
             for key, value in fileSig.items():   # Iterate over File types & Sigs
                 path = os.path.join(root, file)
-                read = open(path, "rb").read(16)  # Read the first 16 bytes in binary
+                sigLen = int(len(value)/2)
+                read = open(path, "rb").read(sigLen)  # Read the first 16 bytes in binary
                 hexBytes = " ".join(['{:02X}'.format(byte) for byte in read])  # Convert binary to hex
-                if hexBytes.startswith(value):   # If file sig is found, append to fileFound dict
+                stripBytes = hexBytes.replace(" ", "")
+                if stripBytes.startswith(value):   # If file sig is found, append to fileFound dict
                     if key in fileFound:
                         fileFound[key].append(file)
                         filePath[key].append(path)
                     else:
                         fileFound[key] = [file]
                         filePath[key] = [path]
-
 
 
 def evidenceGathering():
@@ -132,16 +129,16 @@ def evidenceGathering():
             except shutil.Error as err:  # if duplicate name, file is renamed an then moved
                 homePath = os.path.abspath(os.path.join(__file__, "../../"))  # Gets home directory of application
                 absPath = homePath+"\\"+item
-                fileExtension = os.path.splitext(absPath)[1]
-                fileName = os.path.splitext(absPath)[0]
-                newName = fileName + " " + str(count) + " " + fileExtension  # Stores new filename and path, required as os.rename does not hold data and returns nonetype
-                if key in fileRename:
-                    fileRename[key].append(newName)
-                else:
-                    fileRename[key] = [newName]
-                    os.rename(absPath, fileName + " " + str(count) + " " + fileExtension)  # Rename file to file + date/time
-                    # shutil.move(newName, evidence)
-                    count += 1
+                fileExtension = os.path.splitext(item)[1]
+                fileName = os.path.splitext(item)[0]
+                oldName = fileName + fileExtension
+                newName = fileName + str(count) + fileExtension  # Stores new filename and path, required as os.rename does not hold data and returns nonetype
+                if key not in dupFiles:
+                    dupFiles[key] = {}
+                dupFiles[key].update({oldName: newName})
+                os.rename(absPath, fileName + str(count) + fileExtension)  # Rename file to file + date/time
+                shutil.move(newName, evidence)
+                count += 1
 
 
 def reportGen():
@@ -167,20 +164,45 @@ def reportGen():
 
     for key, value in fileFound.items():
         print("\t\t {} ".format(len(value))+key+" files have been found", file=report)
+    dupTotal = 0
 
-    dupFiles = 0
-    dupTypes = 0
-
-    for key, value in fileRename.items():
+    for key, value in dupFiles.items():
         for files in value:
-            dupFiles += 1
+            dupTotal += 1
 
-    print("\n{} Duplicate Files Have Been Found:".format(dupFiles), file=report)
+    print("\n{} Duplicate Files Have Been Found:".format(dupTotal), file=report)
 
-    for key, value in fileRename.items():
+    for key, value in dupFiles.items():
         print("\t\t{} ".format(len(value))+key+" file(s) have been renamed", file=report)
-        for files in value:
-            print("\t\t\t\t x has been renamed to "+files, file=report)
+        for old, new in value.items():
+            print("\t\t\t\t{} has been renamed to {}".format(old, new), file=report)
+
+    # TODO: Get device, android version, size of backup, total files
+
+
+def deviceInfo():
+    '''Gets information about connected device
+    Backup Size, Device Type, Android Version'''
+    print("\n#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*\n", file=report)
+    print("                  Device Information\n", file=report)
+    print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*\n", file=report)
+    try:
+        serialNo = subprocess.check_output([adb, "shell", "getprop", "ro.serialno"]).decode()
+        print("Serial Number: "+serialNo, file=report)
+        devProduct = subprocess.check_output([adb, "shell", "getprop", "ro.product.device"]).decode()
+        print("Product: "+devProduct, file=report)
+        devModel = subprocess.check_output([adb, "shell", "getprop", "ro.product.model"]).decode()
+        print("Model: "+devModel, file=report)
+        androidVersion = subprocess.check_output([adb, "shell", "getprop", "ro.build.version.release"]).decode()
+        print("Android Version: "+androidVersion, file=report)
+        platPath = os.path.abspath(os.path.join(__file__, "../../android_backup"))
+        backupSize = os.path.getsize(platPath+"/backup.ab")
+        backupSize = (backupSize / 1000000000)
+        print("Backup Size: {} GB ".format(round(backupSize, 2)), file=report)
+        print(type(backupSize))
+    except subprocess.CalledProcessError:
+        print("[ERROR]: No Device Connected", file=report)
+
 
 def main():
     clearFolders()
@@ -188,6 +210,7 @@ def main():
     fileSigAnalysis("rawdump")
     evidenceGathering()
     reportGen()
+
 
 if __name__ == '__main__':
     main()
--- code/test.py
+++ code/test.py
@@ -5,122 +5,50 @@
 
 import os
 import datetime
-import shutil
+import subprocess
 
-report = open("report.txt", "w+")
-dt = datetime.datetime.now().strftime("%d/%m/%y %H:%M")
+report = open("abc123.txt", "w+", 1)
+dt = datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")
+dt2 = datetime.datetime.now()
 
+platPath = os.path.abspath(os.path.join(__file__, "../../platform-tools"))
+adb = platPath+'/adb'
+abe = platPath+'/abe.jar'
+unzip = platPath+'/7za.exe'
 
-# Dictionaries
+fileSig = {"PNG": "89504E47",
+            "JPEG": "FFD8FFE0",
+            "DB": "53514C69746520666F726D6174203300"}
 fileFound = {}
 filePath = {}
-dupFiles = {}
-
-fileSig = {"PNG": "89 50 4E 47",
-                "JPEG": "FF D8 FF E0",
-                "AVI": "52 48 46 46",
-                "DB": "53 51 4C 69 74 65 20 66 6F 72 6D 61 74 20 33 00",
-                "MP3": "49 44 33",
-                "MOV": "6D 6F 6F 76",
-                "TIFF": "49 49 2A",
-                "GIF": 	"47 49 46 38"}
 
 
-def fileSigAnalysis(folder):
-    '''Searches for files within rawdump and matches to stored file filesignature '''
-
-    now = str(datetime.datetime.now())
-    now = now.replace(":", "_")
-
+def deviceInfo(folder):
+    '''Gets information about connected device
+    Backup Size, Device Type, Android Version'''
     for root, directories, files in os.walk(folder):   # For all files in rawdump
         for file in files:
             for key, value in fileSig.items():   # Iterate over File types & Sigs
                 path = os.path.join(root, file)
-                read = open(path, "rb").read(16)  # Read the first 16 bytes in binary
+                sigLen = int(len(value)/2)
+                read = open(path, "rb").read(sigLen)  # Read the first 16 bytes in binary
                 hexBytes = " ".join(['{:02X}'.format(byte) for byte in read])  # Convert binary to hex
-                if hexBytes.startswith(value):   # If file sig is found, append to fileFound dict
+                stripBytes = hexBytes.replace(" ", "")
+                if stripBytes.startswith(value):   # If file sig is found, append to fileFound dict
                     if key in fileFound:
                         fileFound[key].append(file)
                         filePath[key].append(path)
                     else:
                         fileFound[key] = [file]
                         filePath[key] = [path]
+    print(fileFound)
+    print(filePath)
 
-
-
-def evidenceGathering():
-    '''Moves evidence to an evidence folder for each found filetype
-    Renames duplicate files to avoid errors'''
-
-    count = 1
-    for key, value in filePath.items():
-        for item in value:
-            evidence = "evidence/"+key
-            try:  # Make directory if doesnt exist, error raised if it does exist
-                os.makedirs(evidence)
-            except OSError:
-                if not os.path.isdir(evidence):
-                    raise
-            try:  # Moves file to evidence folder
-                shutil.move(item, evidence)
-            except shutil.Error as err:  # if duplicate name, file is renamed an then moved
-                homePath = os.path.abspath(os.path.join(__file__, "../../"))  # Gets home directory of application
-                absPath = homePath+"\\"+item
-                fileExtension = os.path.splitext(item)[1]
-                fileName = os.path.splitext(item)[0]
-                oldName = fileName + fileExtension
-                newName = fileName + str(count) + fileExtension  # Stores new filename and path, required as os.rename does not hold data and returns nonetype
-                if key not in dupFiles:
-                    dupFiles[key] = {}
-                dupFiles[key].update({oldName: newName})
-                os.rename(absPath, fileName + str(count) + fileExtension)  # Rename file to file + date/time
-                shutil.move(newName, evidence)
-                count += 1
-
-
-
-def reportGen():
-    '''Blaa Blaa'''
-
-    print("\n#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*\n", file=report)
-    print("                  File Signature Analysis\n", file=report)
-    print("#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*\n", file=report)
-
-    print("{} file types have been searched for: ".format(len(fileSig)), file=report)
-    for key, value in fileSig.items():
-        print("\t\t"+key, file=report)
-
-    totalFiles = 0
-
-    print("\n{} file types have successfully been found:".format(len(fileFound)), file=report)
-    for key, value in fileFound.items():
-        print("\t\t"+key, file=report)
-        for found in value:
-            totalFiles += 1
-
-    print("\n{} Files have successfully been found:".format(totalFiles), file=report)
-
-    for key, value in fileFound.items():
-        print("\t\t {} ".format(len(value))+key+" files have been found", file=report)
-    dupTotal = 0
-
-    for key, value in dupFiles.items():
-        for files in value:
-            dupTotal += 1
-
-    print("\n{} Duplicate Files Have Been Found:".format(dupTotal), file=report)
-
-    for key, value in dupFiles.items():
-        print("\t\t{} ".format(len(value))+key+" file(s) have been renamed", file=report)
-        for old, new in value.items():
-            print("\t\t\t\t{} has been renamed to {}".format(old, new), file=report)
 
 
 def main():
-    fileSigAnalysis("test")
-    evidenceGathering()
-    reportGen()
-    shutil.rmtree('evidence', ignore_errors=True)
+    deviceInfo("test")
+
 
 if __name__ == '__main__':
     main()
